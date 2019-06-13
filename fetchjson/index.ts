b218b8d0b79c939b2369a099026c482065cd1b30
Yeondam Park <giyen9088@gmail.com>
2019-06-12 23:12:00 -0700

    refactoring the code

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -11,12 +11,14 @@ interface Todo {
 axios.get(url).then(response => {
   const todo = response.data as Todo;
 
-  console.log(todo);
-
   const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.table([
     {
       id,
@@ -24,4 +26,4 @@ axios.get(url).then(response => {
       completed
     }
   ]);
-});
+};
