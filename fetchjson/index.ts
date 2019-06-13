79a763c9aa11442d1740c370d338f78033bb5b81
Yeondam Park <giyen9088@gmail.com>
2019-06-12 22:52:52 -0700

    playing with ts-node

---
+++ fetchjson/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  console.log(todo);
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.table([
+    {
+      id,
+      title,
+      completed
+    }
+  ]);
+});
