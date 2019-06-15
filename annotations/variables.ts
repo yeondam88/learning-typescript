175ebe085f02e7dbab43b28822e86a657114c8ac
Yeondam Park <giyen9088@gmail.com>
2019-06-14 23:39:33 -0700

    when to use annotation or inference

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -28,3 +28,20 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
